6fe2619fc59b6d63a0627d3433dde14a2a1cbb94
vladovel <vladovel@gmail.com>
2018-09-17 09:04:02 +0200

    Initial Commit

---
+++ LBTA_Podcasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBTA_Podcasts
+//
+//  Created by Vlado Velkovski on 9/17/18.
+//  Copyright © 2018 Vlado Velkovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
